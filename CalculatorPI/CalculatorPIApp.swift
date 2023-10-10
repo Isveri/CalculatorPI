1d2c0f209f402b962fc40d3a6c0301c03a235d5e
twighly <blackpanterastudio@gmail.com>
2023-10-10 11:54:59 +0200

    Initial Commit

---
+++ CalculatorPI/CalculatorPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorPIApp.swift
+//  CalculatorPI
+//
+//  Created by student on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
